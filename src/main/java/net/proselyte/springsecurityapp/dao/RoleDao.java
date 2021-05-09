2eff05cecdd58a22980def322d47ef2d9ca807c7
Michael <tyuoc@mail.ru>
2021-05-09 17:06:21 +0300

    test methdod added

--- src/main/java/net/proselyte/springsecurityapp/dao/RoleDao.java
+++ src/main/java/net/proselyte/springsecurityapp/dao/RoleDao.java
@@ -4,5 +4,5 @@ import net.proselyte.springsecurityapp.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleDao extends JpaRepository<Role, Long> {
- 
+ void test();
 }
